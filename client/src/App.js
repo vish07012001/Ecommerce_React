4b5d11ac1cf2869a8ceef3a3d94a42a2b2dcf775
vish07012001 <vishva0701@gmail.com>
2025-06-14 15:06:18 -0400

    solution

--- client/src/App.js
+++ client/src/App.js
@@ -1,88 +1,57 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.js';
-import ProtectedRoute from './ProtectedRoute.js';
-import Admin from './admin/pages/Dashboard.js';
-import Login from './admin/pages/Login.js';
-import Banner from './admin/components/Banner.js';
-import Marquee from './admin/components/Marquee.js';
-import Clothing from './components/categories/Clothing.js'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
+import ProtectedRoute from "./ProtectedRoute.js";
+import Admin from "./admin/pages/Dashboard.js";
+import Login from "./admin/pages/Login.js";
+import Banner from "./admin/components/Banner.js";
+import Marquee from "./admin/components/Marquee.js";
+import Clothing from "./components/categories/Clothing.js";
+import ShopCategories from "./admin/components/ShopCategories.js";
+import ManageProducts from "./admin/components/ManageProducts.js";
+import BestSeller from "./admin/components/BestSeller.js";
+import ShopTheLook from "./admin/components/ShopTheLook.js";
+import SizePicker from "./admin/components/SizePicker.js";
+import ColorPicker from "./admin/components/ColorPicker.js";
+import Header from "./admin/components/Header.js";
+import ShopGramAdmin from "./admin/components/ShopGram.js";
 
-import ShopCategories from './admin/components/ShopCategories.js';
-// import BestSeller from './admin/components/BestSeller.js';
-import ManageProducts from './admin/components/ManageProducts.js';
-
-// import ShopTheLook from './admin/components/ShopTheLook.js'
-import BestSeller from './admin/components/BestSeller.js';
-import ShopTheLook from './admin/components/ShopTheLook.js';
-import SizePicker from './admin/components/SizePicker.js';
-import ColorPicker from './admin/components/ColorPicker.js';
-import Header from './admin/components/Header.js';
-import ShopGramAdmin from './admin/components/ShopGram.js';
 function App() {
-    return (
+  return (
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Login />} />
+        <Route path="/category/clothing" element={<Clothing />} />
 
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/admin/dashboard" element={
-                    <ProtectedRoute>
-                        <Admin />
-                    </ProtectedRoute>} />
-                <Route path="/admin/banner" element={
-                    <ProtectedRoute>
-                        <Banner />
-                    </ProtectedRoute>} />
-                    <Route path="/admin/Header" element={
-                    <ProtectedRoute>
-                        <Header/>
-                    </ProtectedRoute>} />
-                <Route path="/admin/marquee" element={
-                    <ProtectedRoute>
-                        <Marquee />
-                    </ProtectedRoute>} />
-                <Route path="/admin/shopcategories" element={
-                    <ProtectedRoute>
-                        <ShopCategories />
-                    </ProtectedRoute>} /> 
-                    <Route path="/admin/bestseller" element={
-                    <ProtectedRoute>
-                        <BestSeller />
-                    </ProtectedRoute>} /> 
-                    <Route path="/admin/color-picker" element={
-                    <ProtectedRoute>
-                        <ColorPicker />
-                    </ProtectedRoute>} /> 
-                    <Route path="/admin/size-picker" element={
-                    <ProtectedRoute>
-                        <SizePicker />
-                    </ProtectedRoute>} /> 
-                <Route path="/admin/manageproducts" element={
-                    <ProtectedRoute>
-                        <ManageProducts />
-                    </ProtectedRoute>} /> 
+        {/* Protected Admin Routes */}
+        <Route
+          path="/admin/*"
+          element={
+            <ProtectedRoute>
+              <Routes>
+                <Route path="dashboard" element={<Admin />} />
+                <Route path="banner" element={<Banner />} />
+                <Route path="header" element={<Header />} />
+                <Route path="marquee" element={<Marquee />} />
+                <Route path="shopcategories" element={<ShopCategories />} />
+                <Route path="bestseller" element={<BestSeller />} />
+                <Route path="color-picker" element={<ColorPicker />} />
+                <Route path="size-picker" element={<SizePicker />} />
+                <Route path="manageproducts" element={<ManageProducts />} />
+                <Route path="shopgram" element={<ShopGramAdmin />} />
+                <Route path="shopthelook" element={<ShopTheLook />} />
+              </Routes>
+            </ProtectedRoute>
+          }
+        />
 
-                    <Route path="/admin/shopgram" element={
-                    <ProtectedRoute>
-                        <ShopGramAdmin />
-                    </ProtectedRoute>} />    
-
-                    <Route path="/admin/shopthelook" element={
-                    <ProtectedRoute>
-                        <ShopTheLook />
-                    </ProtectedRoute>} />     
-                              
-                    <Route path="/category/clothing" element={
-                    <ProtectedRoute>
-                        <Clothing />
-                    </ProtectedRoute>} />
-
-                <Route path="/admin" element={<Login />} />
-                <Route path="*" element={<h1>Not Found</h1>} />
-                </Routes>
-            </Router>
-
-    );
+        {/* 404 Route */}
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
